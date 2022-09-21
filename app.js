1c3730845028c3a3aaaabe355042efecc8b85e5e
Huszan <mateuszjacenty1@gmail.com>
2022-09-21 18:08:28 +0200

    Update app.js

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ const app = express();
 app.use(cors({ origin: "*" }));
 app.use(bodyParser.json());
 
-app.listen(3000, () => {
-  console.log("The server started on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`The server started on port ${process.env.PORT}`);
 });
 
 app.post("/post", (req, res) => {
