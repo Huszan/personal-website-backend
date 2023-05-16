ef9a04f3a5c0b465f301f221550f1c65d9eb0121
Huszan <mateuszjacenty1@gmail.com>
2023-05-16 11:09:01 +0200

    Password change (temporary)

--- src/modules/account-handler.ts
+++ src/modules/account-handler.ts
@@ -4,6 +4,12 @@ import {User} from "../entity/User";
 const randomstring = require("randomstring");
 const bcrypt = require('bcrypt');
 
+export const forgotPasswordWaitingList: {
+    user: User;
+    newPassword: string;
+    token: string;
+}[] = [];
+
 export function getRandomString() {
     return randomstring.generate();
 }
@@ -27,4 +33,26 @@ export function sendConfirmationEmail(url: string, user: User, callback?: Functi
         },
         callback
     )
+}
+
+export function sendPasswordResetEmail(url: string, token: string, user: User, callback?: Function) {
+    EmailHandler.sendCustomMail(
+        {
+            from: 'Manga-dot',
+            to: user.email,
+            subject: 'Manga-dot: activate account',
+            text: `To reset your account password, please click this link - 
+                                \n${url}?token=${token}`
+        },
+        callback
+    )
+}
+
+export function forgotListIndexOfReq(token: string) {
+    let i = 0;
+    for (let entry of forgotPasswordWaitingList) {
+        if (entry.token === token) { return i; }
+        i++;
+    }
+    return null;
 }
