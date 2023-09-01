473ab748a5a5906456b9ffc4f768e9f81facf061
Huszan <mateuszjacenty1@gmail.com>
2023-09-01 17:30:31 +0200

    Update data-source.ts

--- src/data-source.ts
+++ src/data-source.ts
@@ -10,21 +10,7 @@ import {Page} from "./entity/Page";
 const localDataSource = new DataSource({
     type: "mysql",
     host: "***REMOVED***",
-    port: 3306,
-    username: "root",
-    password: "***REMOVED***",
-    database: "***REMOVED***",
-    synchronize: true,
-    logging: false,
-    entities: [HtmlLocate, Manga, Like, User, Chapter, Page],
-    migrations: [],
-    subscribers: [],
-})
-
-const productionDataSource = new DataSource({
-    type: "mysql",
-    host: "***REMOVED***",
-    port: 3306,
+    port: ***REMOVED***,
     username: "***REMOVED***",
     password: "***REMOVED***",
     database: "***REMOVED***",
