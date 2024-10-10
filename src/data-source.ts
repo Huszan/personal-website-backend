8321d972ed937619dfa043b5aa9a43486b72b39f
Mateusz Jacenty <mateuszjacenty1@gmail.com>
2024-10-10 10:41:10 +0200

    Update data-source.ts

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,59 +1,59 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { HtmlLocate } from "./entity/HtmlLocate"
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { HtmlLocate } from "./entity/HtmlLocate";
 import { Manga } from "./entity/Manga";
-import {Like} from "./entity/Like";
-import {User} from "./entity/User";
-import {Chapter} from "./entity/Chapter";
-import {Page} from "./entity/Page";
+import { Like } from "./entity/Like";
+import { User } from "./entity/User";
+import { Chapter } from "./entity/Chapter";
+import { Page } from "./entity/Page";
 
 const localDataSource = new DataSource({
-    type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "root",
-    password: "***REMOVED***",
-    database: "***REMOVED***",
-    synchronize: true,
-    logging: false,
-    entities: [HtmlLocate, Manga, Like, User, Chapter, Page],
-    migrations: [],
-    subscribers: [],
-})
+  type: "mysql",
+  host: "localhost",
+  port: 3306,
+  username: "root",
+  password: "***REMOVED***",
+  database: "***REMOVED***",
+  synchronize: true,
+  logging: false,
+  entities: [HtmlLocate, Manga, Like, User, Chapter, Page],
+  migrations: [],
+  subscribers: [],
+});
 
 const dsGoogleCloud = new DataSource({
-    type: "mysql",
-    host: "***REMOVED***",
-    port: 3306,
-    username: "admin",
-    password: "***REMOVED***",
-    database: "***REMOVED***",
-    synchronize: true,
-    logging: false,
-    entities: [HtmlLocate, Manga, Like, User, Chapter, Page],
-    migrations: [],
-    subscribers: [],
-})
+  type: "mysql",
+  host: "***REMOVED***",
+  port: 3306,
+  username: "***REMOVED***",
+  password: "***REMOVED***",
+  database: "***REMOVED***",
+  synchronize: true,
+  logging: false,
+  entities: [HtmlLocate, Manga, Like, User, Chapter, Page],
+  migrations: [],
+  subscribers: [],
+});
 
 const dsKamateraBaseMaD = new DataSource({
-    type: "mysql",
-    host: "***REMOVED***",
-    port: 3306,
-    username: "root",
-    password: "***REMOVED***",
-    database: "***REMOVED***",
-    synchronize: true,
-    logging: false,
-    entities: [HtmlLocate, Manga, Like, User, Chapter, Page],
-    migrations: [],
-    subscribers: [],
-})
+  type: "mysql",
+  host: "***REMOVED***",
+  port: 3306,
+  username: "root",
+  password: "***REMOVED***",
+  database: "***REMOVED***",
+  synchronize: true,
+  logging: false,
+  entities: [HtmlLocate, Manga, Like, User, Chapter, Page],
+  migrations: [],
+  subscribers: [],
+});
 
-let dataSource = dsKamateraBaseMaD;
+let dataSource = dsGoogleCloud;
 
-if (process.env.NODE_ENV.trim() === 'development') {
-    console.log('Started data source on development mode');
-    dataSource = localDataSource;
+if (process.env.NODE_ENV.trim() === "development") {
+  console.log("Started data source on development mode");
+  dataSource = localDataSource;
 }
 
 export const AppDataSource = dataSource;
