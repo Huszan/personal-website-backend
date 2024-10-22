67cbdd86892dd9a1d9ed9a6b4223bde54d29a488
Mateusz Jacenty <mateuszjacenty1@gmail.com>
2024-10-22 12:09:31 +0200

    Use request rate limit

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import * as bodyParser from "body-parser";
 import { AdvancedScrapper } from "./modules/scrapper/advanced-scrapper";
 import * as express from "express";
 import * as router from "./routes/_main-router";
+import rateLimit from "express-rate-limit";
 
 const cors = require("cors");
 require("@dotenvx/dotenvx").config();
@@ -10,6 +11,12 @@ require("@dotenvx/dotenvx").config();
 AppDataSource.initialize()
     .then(async () => {
         const app = express();
+        const limiter = rateLimit({
+            windowMs: 1 * 60 * 1000, // 1 minute
+            max: 200,
+            message: "Too many requests, please try again later.",
+        });
+
         let listener: any = app.listen(process.env.PORT || 3000, () => {
             console.log(
                 `The server started on port ${listener.address()!.port}`
@@ -17,6 +24,7 @@ AppDataSource.initialize()
         });
         app.use(bodyParser.json());
         app.use(cors());
+        app.use(limiter);
         router.applyRoutes(app);
 
         AdvancedScrapper.axiosSetup();
